a0323741c088193f64ed66d3f4b62b79f50b706c
Your Name <you@example.com>
2018-10-26 11:15:24 +1100

    add JSON parse

--- web_scrap/lib/scraper.rb
+++ web_scrap/lib/scraper.rb
@@ -4,35 +4,22 @@ require 'json'
 require 'restclient'
 
 
+# response = HTTParty.get("https://www.karenmillen.com/au/womens/sale/?prefn1=refinementcategory&prefv1=Dresses&page=2&format=page-element"))
+html = Nokogiri::HTML(RestClient.get("https://www.karenmillen.com/au/womens/sale/?prefn1=refinementcategory&prefv1=Dresses&page=2&format=page-element"))   
+  
+#CSS selector 
+html.css("ul[data-page]").each do |form|
 
-# class Scraper
+#HTML attribute selector (1 by 1)
+# p JSON.parse(form["data-gtm"])[0]["price"]
+ # p JSON.parse(form["data-gtm"])[0]["name"]
 
-#   attr_accessor :parse_page
-
-#    def initialize
-    # response = HTTParty.get("https://www.karenmillen.com/au/womens/sale/?prefn1=refinementcategory&prefv1=Dresses&page=2&format=page-element")
-    page = Nokogiri::HTML(RestClient.get("https://www.karenmillen.com/au/womens/sale/?prefn1=refinementcategory&prefv1=Dresses&page=2&format=page-element"))   
-
-def parse_page
-  @parse_page ||= Nokogiri::HTML(doc)
+ #HTML attribute selector (looping through the hash to display all items)
+JSON.parse(form["data-gtm"]).each do |hash|
+  puts "The name of the dress is #{hash["name"]} and the price is #{hash["price"]}"
+end
 end
 
-puts page
-
-# puts page.css("title")
-
-# puts Nokogiri::CSS.parse("ul[data-page=2]")
-# ["data_page"][0].text  # => title
-# puts page.css("title")[0].text   # => My webpage
-#   
-
-#     scraper = Scraper.new
-#     names = scraper.get_names
-
-#     puts names 
-
-# # names.each do |index|
-#    puts "Item is #{names |index| }" 
-# # end
-# end
-
+    
+    
+    
